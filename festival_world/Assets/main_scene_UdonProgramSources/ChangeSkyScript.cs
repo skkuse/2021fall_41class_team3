926b84c7821f7d9b2fd715fce2783aa91f883c05
boaaaang <booing68@naver.com>
2021-11-23 16:23:21 +0900

    211123 commit

--- festival_world/Assets/main_scene_UdonProgramSources/ChangeSkyScript.cs
+++ festival_world/Assets/main_scene_UdonProgramSources/ChangeSkyScript.cs
@@ -11,6 +11,10 @@ namespace changesky
 {
     public class ChangeSkyScript : UdonSharpBehaviour
     {
+        public AudioClip[] day_clips;
+        public AudioClip[] night_clips;
+        public AudioSource audioSource;
+
         public Material daysky;
         public Material nightsky;
         public GameObject skymode;
@@ -21,9 +25,74 @@ namespace changesky
         public ParticleSystem firework;
         public ParticleSystem firework2;
 
-      
+        private bool _isActive;
+        private float _timerCount;
+        public float timer = 5;
+
+
+        private AudioClip day_GetRandomClip()
+        {
+            return day_clips[Random.Range(0, day_clips.Length)];
+        }
+
+        private AudioClip night_GetRandomClip()
+        {
+            return night_clips[Random.Range(0, night_clips.Length)];
+        }
+
+        private void Update()
+        {
+            if (_isActive) {
+                if (_timerCount >= timer)
+                {
+                    skymodeActive = skymode.activeSelf;
+                    if (skymodeActive == true)
+                    {
+                        RenderSettings.skybox = nightsky;
+                        skymode.SetActive(false);
+                        day_particle.SetActive(false);
+                        night_particle.SetActive(true);
+                        firework.Play();
+                        firework2.Play();
+                        audioSource.clip = night_GetRandomClip();
+                        audioSource.Play();
+                    }
+                    else
+                    {
+                        RenderSettings.skybox = daysky;
+                        skymode.SetActive(true);
+                        night_particle.SetActive(false);
+                        firework.Stop();
+                        firework2.Stop();
+                        day_particle.SetActive(true);
+                        audioSource.clip = day_GetRandomClip();
+                        audioSource.Play();
+                    }
+                    _timerCount = 0;
+                    _isActive = false;
+                }
+                else {
+                    _timerCount += Time.deltaTime;
+                }
+            }
+            skymodeActive = skymode.activeSelf;
+            if (!audioSource.isPlaying) {
+                if (skymodeActive == true)
+                {
+                    audioSource.clip = day_GetRandomClip();
+                    audioSource.Play();
+                }
+                else {
+                    audioSource.clip = night_GetRandomClip();
+                    audioSource.Play();
+                }
+            }
+        }
+
         public override void Interact()
         {
+            _isActive = true;
+            /*
             skymodeActive = skymode.activeSelf;
             if (skymodeActive == true)  
             {
@@ -33,6 +102,8 @@ namespace changesky
                 night_particle.SetActive(true);
                 firework.Play();
                 firework2.Play();
+                audioSource.clip = night_GetRandomClip();
+                audioSource.Play();
             }
             else
             {
@@ -42,10 +113,15 @@ namespace changesky
                 firework.Stop();
                 firework2.Stop();
                 day_particle.SetActive(true);
+                audioSource.clip = day_GetRandomClip();
+                audioSource.Play();
             }
+            */
 
         }
 
     }
+
+ 
 
 }
