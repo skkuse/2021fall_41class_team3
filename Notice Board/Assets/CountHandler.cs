7517d1a7d7de7705da24321f6469589d0fcda83d
Sanhaa <livia3609@gmail.com>
2021-11-23 22:51:36 +0900

    Notice Board - voting system implementation
    prefab 형태로 저장 완료
    project src 받으면 바로 추가해볼 예정

---
+++ Notice Board/Assets/CountHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountHandler : MonoBehaviour
+{   
+    public string GetValue()
+    {
+        Text txt = transform.Find("Text").GetComponent<Text>();
+        return txt.text;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
