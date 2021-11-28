47c760f2da6d70d2ebdfdc701491b6c9330b2044
Selee <33747749+Lijuet@users.noreply.github.com>
2021-11-28 12:41:56 +0900

    [1128] Video Streaming
    [Main Screen]
    * Screen
    * URL Connect
    * Volume Slider
    * Play/Pause Button
    
    [Mini]
    * Screen
    * Mute Button

---
+++ festival_world/Assets/Scripts/VideoStreaming.cs
@@ -0,0 +1,53 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDK3.Video.Components.AVPro;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class VideoStreaming : UdonSharpBehaviour
+{
+    [SerializeField]
+    private AudioSource miniAudioSource;
+
+    [SerializeField]
+    private AudioSource mainAudioSource;
+
+    [SerializeField]
+    private VRCAVProVideoPlayer targetVideoPlayer;
+
+    [SerializeField]
+    private GameObject videoControlCV;
+
+    private Slider mainVolumeSlider;
+    private Toggle loopToggle;
+
+    private void Start()
+    {
+        mainVolumeSlider = GetComponentInChildren<Slider>();
+        loopToggle = GetComponentInChildren<Toggle>();
+    }
+
+    public void ToggleMute()
+    {
+        miniAudioSource.mute = !miniAudioSource.mute;
+    }
+    public void SetVolume()
+    {
+        mainAudioSource.volume = mainVolumeSlider.value;
+        Debug.Log(mainAudioSource.volume);
+    }
+
+    public void PlayOrStop()
+    {
+        if (!targetVideoPlayer.IsPlaying) targetVideoPlayer.Play();      
+        else targetVideoPlayer.Pause();
+
+        Debug.Log(targetVideoPlayer.IsPlaying);
+    }
+    public void TurnOffVideoControlCV()
+    {
+        videoControlCV.SetActive(false);
+    }
+}
