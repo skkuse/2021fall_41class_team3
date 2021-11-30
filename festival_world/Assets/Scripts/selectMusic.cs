20159b865979077abaa7646514a5818b99b74e79
boaaaang <booing68@naver.com>
2021-11-30 21:14:21 +0900

    AdminBtn

---
+++ festival_world/Assets/Scripts/selectMusic.cs
@@ -0,0 +1,19 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class selectMusic : UdonSharpBehaviour
+{
+    public AudioClip audioClip;
+    public AudioSource audioSource;
+
+
+    public override void Interact()
+    {
+        audioSource.Stop();
+        audioSource.clip = audioClip;
+        audioSource.Play();
+    }
+}
